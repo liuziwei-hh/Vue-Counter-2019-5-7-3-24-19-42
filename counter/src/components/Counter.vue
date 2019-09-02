6918cb4b3d9493e44ef5d2cdbd4699e167d89068
LZW <1312299240@qq.com>
2019-09-02 10:28:15 +0800

    counter

---
+++ counter/src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <p>{{number}}</p>
+    <button v-on:click="clickPlus">{{ click1 }}</button>
+    <button v-on:click="clickMun">{{ click2 }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      number: 0,
+      click1: "+",
+      click2: "-"
+    };
+  },
+  methods: {
+    clickPlus: function() {
+      this.number++;
+    },
+    clickMun: function() {
+      this.number--;
+    }
+  }
+};
+</script>
